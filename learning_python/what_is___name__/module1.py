a7968edd95cbd530652078bcd2b647ef0373d9d0
masahikokamoshita <masahikokamoshita@gmail.com>
2022-08-26 18:51:16 +0900

    refactored

--- learning_python/what_is___name__/module1.py
+++ learning_python/what_is___name__/module1.py
@@ -1,6 +1,7 @@
 def print_hi(name=""):
-    print(f'Hi, {name}')
+    print(f"Hi, {name}")
     print(__name__)
+
 
 print_hi("Masahiko")
 print(__name__)
--- learning_python/what_is___name__/module2.py
+++ learning_python/what_is___name__/module2.py
@@ -1,7 +1,8 @@
 def print_hi(word=""):
-    print(f'Hi, {word}')
+    print(f"Hi, {word}")
     print(__name__)
 
+
 if __name__ == "__main__":
-    print_hi('Masahiko')
+    print_hi("Masahiko")
     print(__name__)
